b66af96b7122a5bff37c8a6c2da42c0b0014fa5a
Alla <molochko.1979@mail.ru>
2023-03-28 22:07:09 +0300

    finish zadacha1_002

--- zadacha_1_002/Program.cs
+++ zadacha_1_002/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+// 456-> 5
+// 782 -> 8
+// 918 -> 1
+int i = int.Parse(Console.ReadLine());
+System.Console.WriteLine(RemoteSecondDigit(i)); 
+
+int RemoteSecondDigit(int number)
+{
+int FirstNumber = number /10;
+int SecondNumber = FirstNumber %10;
+int result = SecondNumber;
+return result;
+}
